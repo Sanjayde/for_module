8e6f510c61f196223517a1c88ac8d5cde51a4b77
Sanjayde <64639608+Sanjayde@users.noreply.github.com>
2021-02-27 08:49:27 +0530

    Update resources.tf

--- resources.tf
+++ resources.tf
@@ -1,7 +1,12 @@
 resource "aws_instance" "check2" {
 
+    count           = "${length(var.inst_name)}"
     instance_type   = "${var.inst_type}"
     subnet_id       = "${var.sub_id}"
     ami             = "${var.ami}"
+    
+    tags = {
+        Name = var.inst_name
+    }
 }
 
